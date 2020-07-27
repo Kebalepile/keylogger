71da8c7df70a5260e11078b7de4d877b49a53b2f
Kebalepile <pimpabutterfly86@gmail.com>
2020-07-27 13:41:35 +0200

    added e.key as fallback for e.keyCode

--- main.js
+++ main.js
@@ -2,7 +2,7 @@
 
 function keylogger(e){
   const key = e.key,
-        keyCode = e.keyCode;
+        keyCode = e.keyCode || e.key;
 
    document.getElementById("key").textContent = key;
    document.getElementById("keyCode").textContent = keyCode;
